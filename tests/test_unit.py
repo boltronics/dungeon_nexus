e3f088c03239c91d78440966b3168c5ce9642ea4
Adam Bolte <abolte@systemsaviour.com>
2023-05-21 20:51:47 +1000

    Test units contain a name

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -8,6 +8,15 @@ import dungeon_nexus.unit
 class UnitTest(unittest.TestCase):
     """Unit tests for the Unit class"""
 
+    def setUp(self):
+        self.unit_name = "Sir Galahad"
+
+    def test_nama(self):
+        """Check the unit exposes a name attribute"""
+        unit = dungeon_nexus.unit.Unit(self.unit_name)
+        self.assertTrue(hasattr(unit, "name"))
+        self.assertIsInstance(unit.name, str)
+
 
 class PartyTest(unittest.TestCase):
     """Unit tests for the Party class"""
